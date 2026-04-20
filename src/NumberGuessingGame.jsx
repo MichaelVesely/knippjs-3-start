a8e8d6415770e392f7575993bb1d6cd07e479764
null <null>
2026-04-20 20:22:49 +0200

    finalized function of game

--- src/NumberGuessingGame.jsx
+++ src/NumberGuessingGame.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react"
+
 import NumberInput from "./components/NumberInput.jsx"
 import Result from "./components/Result.jsx"
 
 function NumberGuessingGame() {
+
+  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * 100 + 1))
+  const [number, setNumber] = useState(50);
+  const [text, setText] = useState("Geben Sie eine Zahl von 1 bis 100 ein.")
+
+  const handleClick = (e) => {
+      if (number > randomNumber) {
+        setText("Die gesuchte Zahl ist kleiner.")
+    } else if (number < randomNumber) {
+        setText("Die gesuchte Zahl ist größer.")
+    } else if (number === randomNumber) {
+        setText("Richtig! Die gesuchte Zahl ist " + number + "!")
+    }
+  }
+
   return (
     <div>
       <h1>Zahlenratespiel</h1>
-      <NumberInput />
-      <Result text="TEXT"/>
+      <NumberInput number={number} setNumber={setNumber} />
+      <button onClick={handleClick}>Zahl prüfen</button>
+      <Result text={text}/>
     </div>
   );
 }
