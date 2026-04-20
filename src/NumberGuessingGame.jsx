ceedc343cdbcafc5311856effba96c5abe25a666
null <null>
2026-04-20 19:42:15 +0200

    imported Result and NumberInput components

--- src/NumberGuessingGame.jsx
+++ src/NumberGuessingGame.jsx
@@ -1,9 +1,12 @@
+import NumberInput from "./components/NumberInput.jsx"
+import Result from "./components/Result.jsx"
+
 function NumberGuessingGame() {
   return (
     <div>
       <h1>Zahlenratespiel</h1>
-      <p>Zahleneingabe</p>
-      <p>Button</p>
+      <NumberInput />
+      <Result text="TEXT"/>
     </div>
   );
 }
