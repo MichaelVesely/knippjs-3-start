c03ea02808a56c5947af53dc159b877888cee03f
null <null>
2026-04-20 20:22:16 +0200

    refactored to show input field

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,5 +1,9 @@
-function NumberInput() {
-    return <p>Zahleneingabe</p>
+function NumberInput({ number, setNumber }) {
+    const changeHandler = (e) => setNumber(e.target.valueAsNumber);
+
+    return (
+        <input type="number" value={number} onChange={changeHandler} />
+    )
 }
 
 export default NumberInput;
